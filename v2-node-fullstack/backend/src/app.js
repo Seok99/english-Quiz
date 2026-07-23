3e9d6c697ceedf93a6237f5e9007777eded02c3c
Seok99 <ydbtjrqja@naver.com>
2026-07-23 16:29:31 +0900

    JWT로그인방식 OTP사용

--- v2-node-fullstack/backend/src/app.js
+++ v2-node-fullstack/backend/src/app.js
@@ -1,6 +1,8 @@
 // src/app.js
 const express = require('express');
 const cors = require('cors');
+//
+const pool = require('./config/db');
 
 const app = express();
 
@@ -10,6 +12,6 @@ app.use(express.json());
 // 테스트용 기본 라우트
 app.get('/', (req, res) => {
   res.json({ message: '서버가 정상적으로 실행 중입니다.' });
-});
+});ㅌ
 
-module.exports = app;
+module.exports = app; 
